07f5694de605b9abc3101ee40e58df57872a833d
PrgDominique <dominiquesimbillo10@gmail.com>
2023-03-04 12:58:54 +0800

    added some style

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
 import { Fetch } from './components/hooks/Fetch'
 
 function App() {
-  const { showData, error, loading, getData } = Fetch('https://v2.jokeapi.dev/joke/Any')
+  const { showData, error, loading, getData } = Fetch(
+    'https://v2.jokeapi.dev/joke/Any'
+  )
 
   console.log(showData)
 
-
   if (error) console.log(error)
   return (
-    <div className='flex flex-col justify-center items-center text-3xl h-screen'>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <>
-        {showData?.joke}
-          {showData?.setup} : {showData?.delivery}
-        </>
-      )}
-      <div className="mt-10 ">
-
-      <button onClick={getData} className='bg-slate-600 border rounded-lg p-2 hover:bg-slate-800 text-white'>Refresh</button>
+    <div className=' h-screen '>
+      <div className='container flex flex-col justify-center items-center text-3xl h-full'>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div className=' m-5'>
+            <div>{showData && <>{showData?.joke} </>}</div>
+            <div>
+              {showData?.setup} {showData?.delivery}
+            </div>
+          </div>
+        )}
+        <div className='mt-10 '>
+          <button
+            onClick={getData}
+            className='bg-slate-600 border rounded-lg p-2 hover:bg-slate-800 text-white'
+          >
+            Refresh
+          </button>
+        </div>
       </div>
     </div>
   )
--- src/components/hooks/Fetch.jsx
+++ src/components/hooks/Fetch.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 export const Fetch = (url) => {
-  const [showData, setShowData] = useState(null)
+  const [showData, setShowData] = useState({})
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
 
