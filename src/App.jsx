68e605ec1d3bdeb53e7ff6b1d5638cf989c7096b
PrgDominique <dominiquesimbillo10@gmail.com>
2023-03-04 09:20:44 +0800

    added loading first before fetch completely the data

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ function App() {
   const { showData, error, loading } = Fetch('https://v2.jokeapi.dev/joke/Any')
 
   console.log(showData)
-  if (loading) return <h1> Loading ... </h1>
 
 
   if (error) console.log(error)
   return (
     <div className='flex justify-center items-center text-3xl h-screen'>
-      {showData ? (
-        showData?.joke
+      {loading ? (
+        <h1>Loading...</h1>
       ) : (
         <>
+        {showData?.joke}
           {showData?.setup} : {showData?.delivery}
         </>
       )}
